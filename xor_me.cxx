35eeec22d732cc8ef59ec56d9dcc80d45b7c9c90
Bruno Michel <bmichel@menfin.info>
2011-06-05 14:09:36 +0200

    Remove the useless arg of xor_me for XLS detection

--- xor_me.cxx
+++ xor_me.cxx
@@ -1,19 +1,12 @@
 #include "binarycodec.cxx"
 
 int main(int argc, char ** argv) {
-
-	oox::core::BinaryCodec_XOR::CodecType algo;
-	unsigned char * pwdToTest = reinterpret_cast<unsigned char *>(argv[1]);
 	if (argc < 4) {
-		std::cout << "Usage: " << argv[0] << " <passwd_to_test> <0xKey> <0xHash> [1 for XLS]" << std::endl;
+		std::cout << "Usage: " << argv[0] << " <passwd_to_test> <0xKey> <0xHash>" << std::endl;
 		return EXIT_FAILURE;
-	} else if (argc >= 5) {
-		algo = oox::core::BinaryCodec_XOR::CODEC_EXCEL;
-		std::cout << "Algorithm XOR MS Excel" << std::endl;
-	} else {
-		algo = oox::core::BinaryCodec_XOR::CODEC_WORD;
-		std::cout << "Algorithm XOR MS Word" << std::endl;
 	}
+
+	unsigned char * pwdToTest = reinterpret_cast<unsigned char *>(argv[1]);
 	unsigned short nKey;
 	std::istringstream issk(argv[2]);
 	issk >> std::hex >> nKey;
@@ -24,7 +17,7 @@ int main(int argc, char ** argv) {
 	std::cout << std::hex << "Key: " << nKey << std::endl;
 	std::cout << std::hex << "Hash: " << nHash << std::endl;
 
-	oox::core::BinaryCodec_XOR x(algo);
+	oox::core::BinaryCodec_XOR x(oox::core::BinaryCodec_XOR::CODEC_WORD);
 	x.initKey(pwdToTest);
 
 	if (x.verifyKey(nKey, nHash)) {
