e354e55f82f17b69a7c1ef797735138baa4347e1
Bruno Michel <bmichel@menfin.info>
2011-06-05 14:46:02 +0200

    Try to optimize a bit lcl* functions

--- brute_force.cxx
+++ brute_force.cxx
@@ -1,9 +1,68 @@
-#include "binarycodec.cxx"
+#include <iostream>
+#include <sstream>
+
+
+inline void lclRotateLeft( unsigned short& rnValue, size_t nBits )
+{
+    rnValue = (rnValue << nBits) | (rnValue >> (sizeof(unsigned short) * 8 - nBits));
+}
+
+inline signed long lclGetLen( const unsigned char* pnPassData, signed long nBufferSize )
+{
+    signed long nLen = 0;
+    while( (nLen < nBufferSize) && pnPassData[ nLen ] ) ++nLen;
+    return nLen;
+}
+
+unsigned short lclGetKey( const unsigned char* pnPassData, signed long nBufferSize )
+{
+    signed long nLen = lclGetLen( pnPassData, nBufferSize );
+    if( nLen <= 0 ) return 0;
+
+    unsigned short nKey = 0;
+    unsigned short nKeyBase = 0x8000;
+    unsigned short nKeyEnd = 0xFFFF;
+    const unsigned char* pnChar = pnPassData + nLen - 1;
+    for( signed long nIndex = 0; nIndex < nLen; ++nIndex, --pnChar )
+    {
+        unsigned char cChar = *pnChar & 0x7F;
+        for( size_t nBit = 0; nBit < 8; ++nBit )
+        {
+            lclRotateLeft( nKeyBase, 1 );
+            if( nKeyBase & 1 ) nKeyBase ^= 0x1020;
+            if( cChar & 1 ) nKey ^= nKeyBase;
+            cChar >>= 1;
+            lclRotateLeft( nKeyEnd, 1 );
+            if( nKeyEnd & 1 ) nKeyEnd ^= 0x1020;
+        }
+    }
+    return nKey ^ nKeyEnd;
+}
+
+unsigned short lclGetHash( const unsigned char* pnPassData, signed long nBufferSize )
+{
+    signed long nLen = lclGetLen( pnPassData, nBufferSize );
+
+    unsigned short nHash = static_cast< unsigned short >( nLen );
+    if( nLen > 0 )
+        nHash ^= 0xCE4B;
+
+    const unsigned char* pnChar = pnPassData;
+    for( signed long nIndex = 0; nIndex < nLen; ++nIndex, ++pnChar )
+    {
+        unsigned short cChar = *pnChar;
+        lclRotateLeft( cChar, nIndex + 1 );
+        nHash ^= cChar;
+    }
+    return nHash;
+}
+
+
 
 int main(int argc, char ** argv) {
 	if (argc < 3) {
 		std::cout << "Usage: " << argv[0] << " <0xKey> <0xHash>" << std::endl;
-		return EXIT_FAILURE;
+		return -1;
 	}
 
 	unsigned short nKey;
@@ -16,35 +75,33 @@ int main(int argc, char ** argv) {
 	std::cout << std::hex << "Key: " << nKey << std::endl;
 	std::cout << std::hex << "Hash: " << nHash << std::endl;
 
-	oox::core::BinaryCodec_XOR x(oox::core::BinaryCodec_XOR::CODEC_EXCEL);
 	unsigned char t[9] = { '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0' };
 
-	// BRUTE FORCE up to 5 chars
+	// BRUTE FORCE up to 6 chars
 	for(unsigned char i=32; i < 127; ++i) {
-		std::cout << (short)i << std::endl;
+		std::cout << "  " << (short)i << std::endl;
 		for(unsigned char j=32; j < 128; ++j) {
-			std::cout << "  " << (short)j << std::endl;
+			std::cout << "    " << (short)j << std::endl;
 			for(unsigned char k=32; k < 128; ++k) {
 				for(unsigned char l=32; l < 128; ++l) {
 					for(unsigned char m=32; m < 128; ++m) {
-						if (nHash == x.getHash(t, 16)) {
-							x.initKey(t);
-							bool res = x.verifyKey(nKey, nHash);
-							if (res) {
-								std::cout << t << " " << res << std::endl;
-								return EXIT_SUCCESS;
+						for(unsigned char n=32; n < 128; ++n) {
+							if (nHash == lclGetHash(t, 16)) {
+								if (nKey == lclGetKey(t, 16)) {
+									std::cout << "Password: " << t << std::endl;
+									return 0;
+								}
 							}
+							t[0] = n;
 						}
-						t[0] = m;
+						t[1] = m;
 					}
-					t[1] = l;
+					t[2] = l;
 				}
-				t[2] = k;
+				t[3] = k;
 			}
-			t[3] = j;
+			t[4] = j;
 		}
-		t[4] = i;
+		t[5] = i;
 	}
-
-	return EXIT_FAILURE;
 }
