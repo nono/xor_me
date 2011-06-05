081ec092da0d95c6d368ca2c40ad827b2ce6ce46
Bruno Michel <bmichel@menfin.info>
2011-06-05 16:45:19 +0200

    It works for passwords with up to 8 chars

--- brute_force.cxx
+++ brute_force.cxx
@@ -76,62 +76,84 @@ int main(int argc, char ** argv) {
 	unsigned short r[9] = { '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0' };
 	unsigned short hash = lclGetHash(t, r, 16);
 
-	// BRUTE FORCE up to 6 chars
+	// BRUTE FORCE up to 8 chars
 	for (unsigned char i=32; i < 127; ++i) {
 		std::cout << "  " << (short)i << std::endl;
 		for (unsigned char j=32; j < 128; ++j) {
+			std::cout << "    " << (short)j << std::endl;
 			for (unsigned char k=32; k < 128; ++k) {
+				std::cout << "      " << (short)k << std::endl;
 				for (unsigned char l=32; l < 128; ++l) {
 					for (unsigned char m=32; m < 128; ++m) {
-						unsigned short n = nHash ^ hash;
-						lclRotateRight(n, 1);
-						if (32 <= n && n < 127) {
-							t[0] = static_cast<unsigned char>(n);
-							if (nKey == lclGetKey(t, 16)) {
-								std::cout << "Password: '" << t << "'" << std::endl;
-								return 0;
+						for (unsigned char n=32; n < 128; ++n) {
+							for (unsigned char o=32; o < 128; ++o) {
+								unsigned short x = nHash ^ hash;
+								lclRotateRight(x, 1);
+								if (32 <= x && x < 127) {
+									t[0] = static_cast<unsigned char>(x);
+									if (nKey == lclGetKey(t, 16)) {
+										std::cout << "Password: '" << t << "'" << std::endl;
+										return 0;
+									}
+								}
+								hash ^= r[1];
+								r[1] = t[1] = o;
+								lclRotateLeft(r[1], 2);
+								hash ^= r[1];
+								if (o == 32) {
+									r[0] = '\0';
+									hash = lclGetHash(t, r, 16);
+								}
+							}
+							hash ^= r[2];
+							r[2] = t[2] = n;
+							lclRotateLeft(r[2], 3);
+							hash ^= r[2];
+							if (n == 32) {
+								r[0] = '\0';
+								hash = lclGetHash(t, r, 16);
 							}
 						}
-						hash ^= r[1];
-						r[1] = t[1] = m;
-						lclRotateLeft(r[1], 2);
-						hash ^= r[1];
+						hash ^= r[3];
+						r[3] = t[3] = m;
+						lclRotateLeft(r[3], 4);
+						hash ^= r[3];
 						if (m == 32) {
 							r[0] = '\0';
 							hash = lclGetHash(t, r, 16);
 						}
 					}
-					hash ^= r[2];
-					r[2] = t[2] = l;
-					lclRotateLeft(r[2], 3);
-					hash ^= r[2];
+					hash ^= r[4];
+					r[4] = t[4] = l;
+					lclRotateLeft(r[4], 5);
+					hash ^= r[4];
 					if (l == 32) {
 						r[0] = '\0';
 						hash = lclGetHash(t, r, 16);
 					}
 				}
-				hash ^= r[3];
-				r[3] = t[3] = k;
-				lclRotateLeft(r[3], 4);
-				hash ^= r[3];
+				hash ^= r[5];
+				r[5] = t[5] = k;
+				lclRotateLeft(r[5], 6);
+				hash ^= r[5];
 				if (k == 32) {
 					r[0] = '\0';
 					hash = lclGetHash(t, r, 16);
 				}
 			}
-			hash ^= r[4];
-			r[4] = t[4] = j;
-			lclRotateLeft(r[4], 5);
-			hash ^= r[4];
+			hash ^= r[6];
+			r[6] = t[6] = j;
+			lclRotateLeft(r[6], 7);
+			hash ^= r[6];
 			if (j == 32) {
 				r[0] = '\0';
 				hash = lclGetHash(t, r, 16);
 			}
 		}
-		hash ^= r[5];
-		r[5] = t[5] = i;
-		lclRotateLeft(r[5], 6);
-		hash ^= r[5];
+		hash ^= r[7];
+		r[7] = t[7] = i;
+		lclRotateLeft(r[7], 8);
+		hash ^= r[7];
 		if (i == 32) {
 			r[0] = '\0';
 			hash = lclGetHash(t, r, 16);
