438286eae8b13e835d8bb0dda51ca12eaa4eaca7
Bruno Michel <bmichel@menfin.info>
2011-06-05 15:31:19 +0200

    Reindent

--- brute_force.cxx
+++ brute_force.cxx
@@ -2,51 +2,51 @@
 #include <sstream>
 
 
-inline void lclRotateLeft( unsigned short& rnValue, size_t nBits )
+inline void lclRotateLeft(unsigned short& rnValue, size_t nBits)
 {
-    rnValue = (rnValue << nBits) | (rnValue >> (sizeof(unsigned short) * 8 - nBits));
+	rnValue = (rnValue << nBits) | (rnValue >> (sizeof(unsigned short) * 8 - nBits));
 }
 
-inline signed long lclGetLen( const unsigned char* pnPassData, signed long nBufferSize )
+inline signed long lclGetLen(const unsigned char* pnPassData, signed long nBufferSize)
 {
-    signed long nLen = 0;
-    while( (nLen < nBufferSize) && pnPassData[ nLen ] ) ++nLen;
-    return nLen;
+	signed long nLen = 0;
+	while ( (nLen < nBufferSize) && pnPassData[ nLen ] ) { ++nLen; }
+	return nLen;
 }
 
-unsigned short lclGetKey( const unsigned char* pnPassData, signed long nBufferSize )
+unsigned short lclGetKey(const unsigned char* pnPassData, signed long nBufferSize)
 {
-    signed long nLen = lclGetLen( pnPassData, nBufferSize );
-    if( nLen <= 0 ) return 0;
+	signed long nLen = lclGetLen(pnPassData, nBufferSize);
+	if ( nLen <= 0 ) return 0;
 
-    unsigned short nKey = 0;
-    unsigned short nKeyBase = 0x8000;
-    unsigned short nKeyEnd = 0xFFFF;
-    const unsigned char* pnChar = pnPassData + nLen - 1;
-    for( signed long nIndex = 0; nIndex < nLen; ++nIndex, --pnChar ) {
-        unsigned char cChar = *pnChar & 0x7F;
-        for( size_t nBit = 0; nBit < 8; ++nBit ) {
-            lclRotateLeft( nKeyBase, 1 );
-            if( nKeyBase & 1 ) nKeyBase ^= 0x1020;
-            if( cChar & 1 ) nKey ^= nKeyBase;
-            cChar >>= 1;
-            lclRotateLeft( nKeyEnd, 1 );
-            if( nKeyEnd & 1 ) nKeyEnd ^= 0x1020;
-        }
-    }
-    return nKey ^ nKeyEnd;
+	unsigned short nKey = 0;
+	unsigned short nKeyBase = 0x8000;
+	unsigned short nKeyEnd = 0xFFFF;
+	const unsigned char* pnChar = pnPassData + nLen - 1;
+	for (signed long nIndex = 0; nIndex < nLen; ++nIndex, --pnChar) {
+		unsigned char cChar = *pnChar & 0x7F;
+		for (size_t nBit = 0; nBit < 8; ++nBit) {
+			lclRotateLeft( nKeyBase, 1 );
+			if (nKeyBase & 1) nKeyBase ^= 0x1020;
+			if (cChar & 1) nKey ^= nKeyBase;
+			cChar >>= 1;
+			lclRotateLeft(nKeyEnd, 1);
+			if (nKeyEnd & 1) nKeyEnd ^= 0x1020;
+		}
+	}
+	return nKey ^ nKeyEnd;
 }
 
-unsigned short lclGetHash( const unsigned char* pnPassData, const unsigned short* pnRotatedData,signed long nBufferSize )
+unsigned short lclGetHash(const unsigned char* pnPassData, const unsigned short* pnRotatedData,signed long nBufferSize)
 {
-    signed long nLen = lclGetLen( pnPassData, nBufferSize );
-    unsigned short nHash = static_cast< unsigned short >( nLen ) ^ 0xCE4B;
+	signed long nLen = lclGetLen(pnPassData, nBufferSize);
+	unsigned short nHash = static_cast<unsigned short>(nLen) ^ 0xCE4B;
 
-    const unsigned short* pnChar = pnRotatedData;
-    for( signed long nIndex = 0; nIndex < nLen; ++nIndex, ++pnChar ) {
-        nHash ^= *pnChar;
-    }
-    return nHash;
+	const unsigned short* pnChar = pnRotatedData;
+	for(signed long nIndex = 0; nIndex < nLen; ++nIndex, ++pnChar) {
+		nHash ^= *pnChar;
+	}
+	return nHash;
 }
 
 
@@ -71,14 +71,14 @@ int main(int argc, char ** argv) {
 	unsigned short r[9] = { '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0' };
 
 	// BRUTE FORCE up to 6 chars
-	for(unsigned char i=32; i < 127; ++i) {
+	for (unsigned char i=32; i < 127; ++i) {
 		std::cout << "  " << (short)i << std::endl;
-		for(unsigned char j=32; j < 128; ++j) {
+		for (unsigned char j=32; j < 128; ++j) {
 			std::cout << "    " << (short)j << std::endl;
-			for(unsigned char k=32; k < 128; ++k) {
-				for(unsigned char l=32; l < 128; ++l) {
-					for(unsigned char m=32; m < 128; ++m) {
-						for(unsigned char n=32; n < 128; ++n) {
+			for (unsigned char k=32; k < 128; ++k) {
+				for (unsigned char l=32; l < 128; ++l) {
+					for (unsigned char m=32; m < 128; ++m) {
+						for (unsigned char n=32; n < 128; ++n) {
 							if (nHash == lclGetHash(t, r, 16)) {
 								if (nKey == lclGetKey(t, 16)) {
 									std::cout << "Password: " << t << std::endl;
